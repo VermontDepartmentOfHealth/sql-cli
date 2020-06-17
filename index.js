4ea9244d1873a598782a100b48ff828dd3085531
john.mathias <john.mathias@vermont.gov>
2020-06-17 13:18:57 -0400

    implementing table comparisons WIP

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ require("dotenv").config()
 const { program } = require('commander');
 program
 ///.command('compare-output', 'compare outputs of two sql statements')
-    .requiredOption('-sv, --server <value>', 'server name')
-    .requiredOption('-db, --database <value>', 'database name')
+    .option('-sv, --server <value>', 'server name')
+    .option('-db, --database <value>', 'database name')
     .option('-s, --source <value>', 'plain text sql statement')
     .option('-t, --target <value>', 'plain text sql statement')
     .parse(process.argv);
@@ -56,6 +56,8 @@ async function main() {
 
         let isIdentical = isNaiveIdentical(recordsetsSource, recordsetsTarget)
 
+        let isNewIdentical = isTableIdentical(recordsetsSource, recordsetsTarget)
+
         console.log('source query: ', program.source)
         console.log('target query: ', program.target)
         console.log('source result: ', recordsetsSource)
@@ -68,6 +70,69 @@ async function main() {
     } catch (err) {
         console.log(err)
     }
+}
+//let isIdentical = 
+//"Are tables identical?"" can be simpler than "Are objects identical?"
+function isTableIdentical(resultOne,resultTwo){
+    //Stored procedures could return multiple tables, method will assume we only get one for now
+    tableOne = resultOne[0]
+    tableTwo = resultTwo[0]
+
+    //This might not be right, the table itselft might be the first item on this array, i'll have to access a storeed
+    //procedure that returns more than one row to find out
+    let tableOneContainsRows = Array.isArray(tableOne) && tableOne.length >= 1
+    let tableTwoContainsRows = Array.isArray(tableTwo) && tableTwo.length >= 1
+
+    //If both tables are empty we will say they are identical
+    if(!tableOneContainsRows && !tableTwoContainsRows){
+        return true;
+    }
+
+    //If one table contains rows and the others does not, they are not identical
+    if(tableOneContainsRows !== tableTwoContainsRows){
+        return false;
+    } 
+
+    //check if both tables contain items
+    //tables have same number of colums, and same column headers, (Im assuming that order of columns should matter, but maybe thats a bad assumption)
+    //let numberOfColumsIntableOne = Object.keys(tableOne[0]).length
+
+    //let numberOfColumsIntableTwo = Object.keys(tableTwo[0]).length
+    let columnsAreIdentical = columnsAreIdenitical(tableOne,tableTwo);
+
+    // let tableOneColumns = Object.keys(tableOne[0])
+    // let tableTwoColumns = Object.keys(tableTwo[0])
+
+    // if(tableOneColumns.length === tableTwoColumns.length){
+    //     return tableOneColumns.every(function(prop){
+    //         return tableTwoColumns.indexOf(prop) >= 0;
+    //     });
+    // }
+    //tables have same number of rows
+    //collection of rows are identical
+    //go row by row, look for collection of rows that have the same value in first column
+    //from this collection find the collection of rows that have the same value in the next colummn
+    //repeat this process (use recursion that looks for final column? you could also just iterate over columns
+    //
+    
+    
+    
+    //for speed, look for a primary key column, I think if we can say that there is a row that has unique values for 
+    //each row than we can use it as a primary key, at least for this operation. 
+
+    return true;
+}
+
+function columnsAreIdenitical(tableOne, tableTwo){
+    let tableOneColumns = Object.keys(tableOne[0])
+    let tableTwoColumns = Object.keys(tableTwo[0])
+
+    if(tableOneColumns.length === tableTwoColumns.length){
+        return tableOneColumns.every(function(prop){
+            return tableTwoColumns.indexOf(prop) >= 0;
+        });
+    }
+    return false;
 }
 
 
